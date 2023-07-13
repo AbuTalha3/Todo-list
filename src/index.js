bd3663983fb325075653b8d1dc0568fc6c3150ac
AbuTalha3 <127955219+AbuTalha3@users.noreply.github.com>
2023-07-13 23:17:17 +0500

    Updated to do list

--- src/index.js
+++ src/index.js
@@ -1,1 +1,97 @@
 import './style.css';
+
+const textInputField = document.querySelector('#text-input-feild');
+const addButton = document.querySelector('#add-button');
+const todosContainer = document.querySelector('.todos-container');
+
+const todos = [];
+
+addButton.addEventListener('click', () => {
+    if (textInputField.value.trim().length == '') {
+        return;
+    }
+
+    // create todo item
+    const todoItem = {
+        text: textInputField.value,
+        completed: false,
+    };
+
+    // add todo item to array
+    todos.push(todoItem);
+
+    // clear text input field
+    textInputField.value = '';
+
+    // render todo item
+    renderTodoItem(todoItem);
+});
+
+function renderTodoItem(todoItem) {
+    // create div add class todo-item-container
+    const todoItemContainer = document.createElement('div');
+    todoItemContainer.classList.add('todo-item-container');
+
+    todosContainer.appendChild(todoItemContainer);
+
+    // create checkbox
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.id = 'todo-checkbox';
+    checkbox.checked = todoItem.completed;
+    todoItemContainer.prepend(checkbox);
+
+    // craete p element and Id = todo-text
+    const todoText = document.createElement('p');
+    todoText.Id = 'todo-text';
+    todoText.innerText = todoItem.text;
+    todoItemContainer.appendChild(todoText);
+
+    // add double click to todo text
+    todoText.addEventListener('dblclick', () => {
+        todoItem.completed = !todoItem.completed;
+        checkbox.checked = todoItem.completed;
+    });
+
+    // create button add id = edit = button
+    const editButton = document.createElement('button');
+    editButton.id = 'edit-button';
+    // create fa-edit icon
+    const editIcon = document.createElement('i');
+    editIcon.classList.add('fas', 'fa-edit');
+    editButton.appendChild(editIcon);
+    todoItemContainer.appendChild(editButton);
+
+    // add click event ti edit button here
+    editButton.addEventListener('click', () => {
+        textInputField.value = todoText.innerText;
+        const parent = editButton.parentElement;
+        parent.parentElement.removeChild(parent);
+    });
+
+    // create button add id = delete-button
+    const deleteButton = document.createElement('button');
+    deleteButton.id = 'delete-button';
+
+    // create fa-trash icon
+    const deleteIcon = document.createElement('i');
+    deleteIcon.classList.add('fas', 'fa-trash');
+    deleteButton.appendChild(deleteIcon);
+    todoItemContainer.appendChild(deleteButton);
+
+    // add click event for deleteButton
+    deleteButton.addEventListener('click', () => {
+        const parent = deleteButton.parentElement;
+        parent.parentElement.removeChild(parent);
+    });
+}
+
+const clearButton = document.querySelector('.clearer');
+
+clearButton.addEventListener('click', () => {
+    const completedItems = document.querySelectorAll('.todo-item-container input[type="checkbox"]:checked');
+    completedItems.forEach((item) => {
+        const parent = item.parentElement;
+        parent.parentElement.removeChild(parent);
+    });
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,36 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
- mode: 'development',
- entry: {
-   index: './src/index.js',
- },
- plugins: [
-   new HtmlWebpackPlugin({
-     template: './src/index.html',
-   }),
- ],
- output: {
-   filename: '[name].bundle.js',
-   path: path.resolve(__dirname, 'dist'),
-   clean: true,
- },
- module: {
-   rules: [
-     {
-       test: /\.css$/i,
-       use: ['style-loader', 'css-loader'],
-     },
-   ],
- },
+  mode: 'development',
+  entry: {
+    index: ['./src/index.js', 'webpack-dev-server/client'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
+    },
+    watchFiles: ['src/**/*', 'dist/**/*'],
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
 };
